7eac48103a90191de1de6800527b764ca73002b0
Alexandra Vlasov <sashilda.vlasov@gmail.com>
2018-09-10 12:37:42 +0300

    Extract API_KEY to constant

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 from telegram.ext import Updater
 
+API_KEY = '688721336:AAFy8ftMA6UZlFumVUSncQGMm79NAuHGJbA'
 
 def main():
-    mybot = Updater('688721336:AAFy8ftMA6UZlFumVUSncQGMm79NAuHGJbA')
+    mybot = Updater(API_KEY)
     mybot.start_polling()
     mybot.idle()
        
