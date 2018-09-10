e63f7c0c46b0f9a748bf20ef4374e1aca786e78b
Alexandra Vlasov <sashilda.vlasov@gmail.com>
2018-09-10 13:24:41 +0300

    command handler for  /start

--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
 from telegram.ext import Updater, CommandHandler      
 from os import environ
+import pprint
 
 # Run in cmd line "source secrets.txt" before running
 API_KEY = environ.get('API_KEY') 
 
 def greet_user(bot, update):
     print('Вызван /start')
-    
+    # pprint method print a dictionary in a column
+    pprint.pprint(update.to_dict())
+
 def main():
     mybot = Updater(API_KEY)
     mydisp = mybot.dispatcher
